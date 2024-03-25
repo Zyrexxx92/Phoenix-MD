95d75594887882b84cf3f966cd29ce5bdf52ac77
7ucg <161209670+7ucg@users.noreply.github.com>
2024-03-25 02:29:53 +0100

    update

--- Configurations.js
+++ Configurations.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "49491741711168", "4365022989060";   // You can replace this number with yours //
+  gg = "49491741711168", "4365022989060", "436502298906101";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://baron:xjFQyvqxnup6vKfQ@lionbot.ymq2zpo.mongodb.net/?retryWrites=true&w=majority&appName=LionBot";
-global.sessionId = process.env.SESSION_ID || "22_3_2c9e_cddd_fded";
+global.mongodb = process.env.MONGODB || "mongodb+srv://baron:baron2006@lionbot.ymq2zpo.mongodb.net/?retryWrites=true&w=majority&appName=LionBot";
+global.sessionId = process.env.SESSION_ID || "phoenix";
 global.prefa = process.env.PREFIX || "/";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Phoenix Bot`;
