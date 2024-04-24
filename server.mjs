dba3de87198dd632aacb06e61eb36da8b83c8dc7
7ucg <161209670+7ucg@users.noreply.github.com>
2024-04-24 16:47:41 +0200

    UPDATE

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Queue from 'queue';
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
 app.get('/', (req, res) => {
   res.render('index.html', { title: 'Bot Online' });
